92e31f95dda25939362a64bb35bbb160c79c75c2
Neto <neto@NetonoMacBook-Pro.local>
2021-08-27 19:03:02 +0900

    changed esper binary

--- challenges/esper/src/esper.c
+++ challenges/esper/src/esper.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int func(){
 	puts("hum...");
@@ -29,7 +30,7 @@ int main()
 	puts("This is second step!");
 	puts("Do you know Logical Operation?");
 	scanf("%d", &number);
-	if (number != a&b+a|b) {puts("Bye"); return 0;}
+	if (number != (a&b)+(a|b)) {puts("Bye"); return 0;}
 	puts("This is last step!");
 	puts("One more Esper check!");
 	int x = 11, y = 20, z = 33;
